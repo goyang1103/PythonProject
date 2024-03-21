f0c35c421acf644f9fc1bf253150cc0352f2d858
BSJ <00bsj@naver.com>
2024-03-21 10:28:09 +0900

    240321 파이썬

--- O320응용예제1.py
+++ O320응용예제1.py
@@ -3,8 +3,8 @@
 
 # import random
 # lotto = []
-# while True:
-#     rand = random.randrange(1, 45)
+# while True:  # 반복 횟수를 알 수 없기 때문에 True, 반드시 탈출 조건 필요
+#     rand = random.randrange(1, 46)
 #     if rand not in lotto:
 #         lotto.append(rand)
 #     if len(lotto) == 6:
@@ -12,13 +12,21 @@
 # print(lotto)
 
 # 임의의 수를 연속(공백) 으로 입력 받아 홀수, 짝수 리스트로 나누어 담기
+# num = list(map(int, input().split()))
+# holl = []
+# zzak = []
+# for e in num:  # 리스트의 요소를 한개씩 끄집어 내면서 순회 (시작 부터 끝까지 자동 순회)
+#     if e % 2 == 0:
+#         zzak.append(e)
+#     else:
+#         holl.append(e)
+# print(f"홀수 : {holl}")
+# print(f"짝수 : {zzak}")
+
+
+# 다른 방법
 num = list(map(int, input().split()))
-holl = []
-zzak = []
-for e in num:
-    if e % 2 == 0:
-        zzak.append(e)
-    else:
-        holl.append(e)
-print(f"홀수 : {holl}")
-print(f"홀수 : {zzak}")
+even = list(filter(lambda x:x % 2 == 0, num))
+odd = list(filter(lambda x:x % 2 == 1, num))
+print(f"홀수 : {odd}")
+print(f"짝수 : {even}")
