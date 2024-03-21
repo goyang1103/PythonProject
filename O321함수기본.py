7b5af2a833f055ace53e6932881696400610ee81
BSJ <00bsj@naver.com>
2024-03-21 14:41:33 +0900

    240321 파이썬

---
+++ O321함수기본.py
@@ -0,0 +1,11 @@
+def name_card(name, addr, phone):  # 매개 변수가 있는 함수, 반환 값은 없음
+    print(f"주소 : {addr}")
+    print(f"전화번호 : {phone}")
+    print(f"이름 : {name}")
+    print('-'*30)
+
+
+name_card("안유진", "서울시 강남구 역삼동", "010-1234-5678")
+name_card("장원영", "서울시 광진구 중곡동", "010-9816-9994")
+name_card("최삡뺩", "고양시 일산동구 장항동", "010-7759-3669")
+
